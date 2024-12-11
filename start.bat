fedfaf38fe7db1321be408d65a9408663311a8d6
GoToLoop <EMLF@HotMail.com>
2024-12-11 20:23:32 -0300

    start.bat w/ Python

--- start.bat
+++ start.bat
@@ -1,3 +1,10 @@
-set /a port = %random% * 40000 / 32768 + 10000
-start servor . index.html %port%
+@echo off
+
+REM Generate a random port between 10000 and 50000
+set /a port=%random% * 40000 / 32768 + 10000
+
+REM Open the default web browser to the local server URL
 start http://localhost:%port%
+
+REM Start a simple HTTP server with Python
+python -m http.server %port%
